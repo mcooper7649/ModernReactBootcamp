e85a3f149165d423051791e3a3db49dde600e113
mcooper7649 <55610025+mcooper7649@users.noreply.github.com>
2021-06-14 18:17:04 -0400

    added copyright and updated metatags

--- lottery-exercise/src/App.js
+++ lottery-exercise/src/App.js
@@ -11,9 +11,10 @@ class App extends Component {
           <meta charset="utf-8" />
           <meta name="viewport" content="width=device-width, initial-scale=1" />
           <meta name="theme-color" content="#000000" />
-          <meta name="author" content="Michael Cooper" />
           <meta name="image" property="og:image" content="https://image.prntscr.com/image/pegFvHJQTMasA7VIqA9dzA.png" />
-          <meta name="description" property="og:description" content="Lucky Lotta Generator App" />
+          <meta name="description" property="og:description" content="Lucky Lotta Generator App| An application for people who are sick of having to think of their own numbers" />
+          <meta name="author" content="Michael Cooper" />
+          <meta name="publish_date" property="og:publish_date" content="2021-06-14T00:00:00-0600"></meta>
           <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@1,100&display=swap" rel="stylesheet" /> 
           <script src="https://kit.fontawesome.com/8cc1b75eb3.js" crossorigin="anonymous"></script>
           <title>Lottery App</title>
@@ -37,6 +38,7 @@ class App extends Component {
           rules="10 balls total, with a Ball Maximum of 80"
 
           />
+          <p className="copyright">Crafted by a <i className="fas fa-user-ninja fa-2x"></i><a className="copyright" href="http://www.mycodedojo.com"> © 2021 MyCodeDojo</a></p>
       </div>
     )
   }
