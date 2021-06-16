d2862c9511114a1f0dba8f57bee009c0577b6120
mcooper7649 <55610025+mcooper7649@users.noreply.github.com>
2021-06-15 22:16:43 -0400

    added meta og:url

--- coin-flipper/src/App.js
+++ coin-flipper/src/App.js
@@ -13,6 +13,7 @@ class App extends Component {
           <meta name="viewport" content="width=device-width, initial-scale=1" />
           <meta name="theme-color" content="#000000" />
           <meta name="image" property="og:image" content="https://image.prntscr.com/image/C9t5t52OSkK_7Hxx1SaYQw.png" />
+          <meta name="url" property="og:url" content="https://image.prntscr.com/image/C9t5t52OSkK_7Hxx1SaYQw.png" />
           <meta name="description" property="og:description" content="Coin Flipper | A Many Faced Coin Flipping Application" />
           <meta name="author" content="Michael Cooper" />
           <meta name="publish_date" property="og:publish_date" content="2021-06-15T00:00:00-0600"></meta>
